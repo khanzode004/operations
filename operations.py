b56775105655e799ecc4736238ef3fb0dd84afd1
Vikram Khanzode <37948284+khanzode004@users.noreply.github.com>
2022-09-13 22:51:37 -0400

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,15 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+summation = a + b
+difference = a-b
+product = a*b
+division = a/b
+power = a**b
+
+print("Sum is:",summation)
+print("Difference is:",difference)
+print("Product is:",product)
+print("Division is:",division)
+print("Power is:",power)
+
